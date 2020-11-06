3e820bbb92b61e3834df451efd307b9d5fff04ac
Sarah DeBey <sarah.debey@gmail.com>
2020-11-06 12:52:23 -0800

    sets up project architecture, opens browser to initial url

---
+++ browser.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+
+async function startBrowser() {
+  let browser;
+
+  try {
+    browser = await puppeteer.launch({
+      headless: false,
+      args: ["--disable-setuid-sandbox"],
+      'ignoreHTTPSErrors': true
+    });
+
+  } catch(err) {
+    console.error(`Could not start browser: ${err}`)
+  }
+  return  browser;
+}
+
+module.exports = startBrowser;
