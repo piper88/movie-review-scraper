4ac40dd91c83aeec8da14c28db1af9082a94b668
Sarah DeBey <sarah.debey@gmail.com>
2020-11-06 15:27:16 -0800

    searches movie title, navigates to new page, and scrapes all the post headlines on new page

--- pageScraper.js
+++ pageScraper.js
@@ -11,14 +11,24 @@ scraper = {
     await page.goto(this.url, {waitUntil: 'load', timeout: 0});
 
     //wait until search box loads
+    await page.waitFor('form#search');
 
     //type in movie title
+    await page.click('input[name=q]');
+    await page.$eval('input[name=q]', el => el.value = 'Borat 2')
+    //limit search to subreddit
+    await page.click('input[name=restrict_sr]')
+    //submit search
+    await page.click('input[type=submit]');
 
-    //get titles of all posts, analyze for nlp
-    //aggregate score, return
-
-    await page.waitFor('form.search');
-  
+    await page.waitForNavigation()
+    let postTitles = await page.$$eval('.search-result', posts => {
+      posts = posts.map(post => {
+        return post.querySelector('div > header > a').innerText;
+      })
+      return posts;
+    })
+    console.log(postTitles);
 
   }
 }
