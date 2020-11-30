5182e9c2a472d1fa16fc65c1224e38a4bb5c3504
Sarah DeBey <sarah.debey@gmail.com>
2020-11-29 17:38:58 -0800

    navigates to all pages with name of movie in title of review

--- pageScraper.js
+++ pageScraper.js
@@ -5,6 +5,53 @@ scraper = {
   async scrape(browserInstance) {
     debug('scraping');
 
+    let allPostTitles = [];
+
+    async function scrapeCurrentPage() {
+
+      console.log('SCRAPER BIKEEEE')
+
+      let postTitles = await page.$$eval('.search-result', posts => {
+        posts = posts.map(post => {
+          return post.querySelector('div > header > a').innerText;
+        })
+        return posts;
+      })
+      postTitles.map(title => allPostTitles.push(title));
+
+
+
+      let nextPage = await page.evaluate(() => {
+        console.log('page being evaluated for next button')
+        console.log(document.querySelector('.nextprev > a[rel~=next]').href);
+        if (document.querySelector('.nextprev > a[rel~=next]')) {
+          console.log('next button');
+           return document.querySelector('.nextprev > a[rel~=next]').href;
+        } else {
+          console.log('no next button');
+          return false;
+        }
+      })
+
+      if (nextPage) {
+        console.log(`next page ${nextPage}`);
+
+        await page.goto(nextPage, {waitUntil: 'load', timeout: 0});
+          //When just kept going to second page for infinity: I was going to the second page, but 'page' was still the first page, so the first page just kept being scraped. Have to change page to the new page, then scrape that new page.
+          //Old non-working code
+          // let nextPage = await browser.newPage();
+          // await nextPage.goto(nextUrl)
+
+        // await page.waitForNavigation();
+        //recursive call to scrape the next page
+        await scrapeCurrentPage();
+      } else {
+        console.log('else');
+        console.log(allPostTitles);
+        return;
+      }
+    }
+
     browser = await browserInstance;
     let page = await browser.newPage();
 
@@ -15,21 +62,16 @@ scraper = {
 
     //type in movie title
     await page.click('input[name=q]');
-    await page.$eval('input[name=q]', el => el.value = 'Borat 2')
+    await page.$eval('input[name=q]', el => el.value = 'Alien')
     //limit search to subreddit
     await page.click('input[name=restrict_sr]')
     //submit search
     await page.click('input[type=submit]');
 
     await page.waitForNavigation()
-    let postTitles = await page.$$eval('.search-result', posts => {
-      posts = posts.map(post => {
-        return post.querySelector('div > header > a').innerText;
-      })
-      return posts;
-    })
-    console.log(postTitles);
+    await scrapeCurrentPage();
 
+    console.log(`allPostTitles ${allPostTitles}`);
   }
 }
 
