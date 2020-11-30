33d86310af7651c68a96d0c7961a38b22bc8b8bb
Sarah DeBey <sarah.debey@gmail.com>
2020-11-29 19:22:10 -0800

    removes special characters, spelling mistakes, and stop words from reviews

--- pageController.js
+++ pageController.js
@@ -2,8 +2,14 @@ const fs = require('fs');
 const pageScraper = require('./pageScraper.js');
 const browser = require('./browser.js');
 
+
 const natural = require('natural');
 const aposToLexForm = require('apos-to-lex-form');
+
+const SpellCorrector = require('spelling-corrector');
+const spellingCorrector = new SpellCorrector();
+spellingCorrector.loadDictionary();
+const stopwords = require('stopword');
 
 controller = {
   async scrapeAll(browserInstance) {
@@ -14,8 +20,24 @@ controller = {
   },
 
   processValence(reviews) {
-    let lexedReviews = aposToLexForm(reviews);
-    console.log(lexedReviews);
+    console.log('YOOOOOOO');
+    //remove contractions (e.g. I don't to I do not)
+    let lexedReviews = aposToLexForm(reviews).toLowerCase();
+    //remove special characters
+    const alphaOnlyReview = lexedReviews.replace(/[^a-zA-Z\s]+/g, '')
+    //Split text into meaningful units
+    const { WordTokenizer } = natural;
+    const tokenizer = new WordTokenizer();
+    const tokenizedReview = tokenizer.tokenize(alphaOnlyReview);
+
+    //correct spelling mistakes
+    tokenizedReview.forEach((word, index) => {
+      tokenizedReview[index] = spellingCorrector.correct(word)
+    })
+    //remove stop words (e.g. but/a/the)
+    const meaningfulReview = stopwords.removeStopwords(tokenizedReview);
+    console.log(meaningfulReview);
+
   }
 }
 
